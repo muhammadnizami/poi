9d7f197f3730fad173b068d9895b50a957a570c7
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-04-20 11:31:16 +0700

    spek poi_file_create_new() di file-manager.h ditulis

---
+++ src/file-manager-test.c
@@ -0,0 +1,5 @@
+#include "file-manager.h"
+
+int main(){
+
+}
--- src/file-manager.h
+++ src/file-manager.h
@@ -5,10 +5,6 @@
 #include <stdint.h>
 
 #define POI_BLOCK_SIZE 512
-#define POI_BLOCK_NUM_MAX 65752
-#define POI_BLOCK_NUM_MIN 0
-
-#define POI_FILE_SIZE (POI_BLOCK_NUM_MAX-POI_BLOCK_NUM_MIN+1)*POI_BLOCK_SIZE
 
 /* ------------------------------------------------ */
 /* --------STRUKTUR DATA BLOK PEMBACAAN BERKAS----- */
@@ -31,20 +27,53 @@ void set_poi_file_block_dword_little_endian(poi_file_block *, uint32_t offset, u
 /* ------------------------------------------------ */
 
 
+
+
+/* ------------------------------------------------ */
+/* -----------FILE POI----------------------------- */
 typedef uint32_t poi_file_block_num;
 
 FILE * dotpoi_mounted_file;
 
+#define POI_VOLUME_INFORMATION_BLOCKS_NUM 1
+#define POI_ALLOCATION_TABLE_BLOCKS_NUM 256
+#define POI_DATA_POOL_BLOCKS_NUM 65536
+
+#define POI_TOTAL_BLOCKS_NUM POI_VOLUME_INFORMATION_BLOCKS_NUM+POI_ALLOCATION_TABLE_BLOCKS_NUM+POI_DATA_POOL_BLOCKS_NUM
+
+
+#define POI_BLOCK_NUM_MIN 0
+#define POI_BLOCK_NUM_MAX TOTAL_BLOCKS_NUM+POI_BLOCK_NUM_MIN-1
+
+
+#define POI_FILE_SIZE (POI_TOTAL_BLOCKS_NUM)*POI_BLOCK_SIZE
+/* ------------------------------------------------ */
+
+
+
 /* ------------------------------------------------ */
 /* PROSEDUR DAN FUNGSI UNTUK MEMULAI DAN MENGAKHIRI */
-
-int poi_file_create_new ( const char * path);
 
 int poi_file_verify (const char * path);
 
 int poi_file_open (const char * path);
 
 int poi_file_close();
+
+
+
+//membuat sebuah berkas berukuran POI_FILE_SIZE yang berisi volume poi kosong
+//mengisi 4 byte pertama dengan magic string "poi!" (tanpa kutipan)
+//mengisi 32 byte dari offset 0x04 dengan Sebuah null-terminated string yang menyimpan nama dari volume. Karakter yang digunakan ditulis dalam format yang dapat dibaca oleh manusia. Jika tak diisikan maka secara default akan berisi "POI!".
+//mengisi 4 byte dari offset 0x24 dengan Kapasitas keseluruhan filesystem dalam blok (Dalam hal ini POI_TOTAL_BLOCKS_NUM), ditulis dalam integer 32-bit little endian. 
+//mengisi 4 byte dari offset 0x28 dengan jumlah blok yang belum terpakai, ditulis dalam integer 32-bit little endian
+//mengisi 4 byte dari offset 0x2C dengan indeks blok pertama yang bebas, ditulis dalam integer 32-bit little endian
+//mengisi 32 byte dari offset 0x30 dengan entri blok direktori untuk direktori root
+//mengisi 428 byte dari offset 0x50 dengan nilai '\0'
+//mengisi 4 byte dari offset 0x1FC dengan string "!iop" (tanpa kutipan)
+//keluar fungsi dalam keadaan file tertutup
+//melempar nilai 1 bila sukses, melempar kode error bila tidak sukses
+int poi_file_create_new ( const char * path);
 
 /* ------------------------------------------------ */
 
