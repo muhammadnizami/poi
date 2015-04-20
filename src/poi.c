99347532c2b054aae426f7ac938d5a9f8127cbfd
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-04-20 07:52:36 +0700

    first commit

---
+++ src/poi.c
@@ -0,0 +1,18 @@
+#include <fuse.h>
+static struct fuse_operations poi_oper = {
+.getattr = ,
+.readdir = ,
+.mkdir = ,
+.mknod = ,
+.read = ,
+.rmdir = ,
+.unlink = ,
+.rename = ,
+.write = ,
+.truncate = ,
+};
+
+int main(){
+
+}
+
